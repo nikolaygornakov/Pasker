5295e17adf986305f5650882f376c924efe030cb
Tanyo Georgiev <tanyogeorgiev@yahoo.com>
2016-12-05 23:38:50 +0200

    Heaader, navigation design changes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link } from 'react-router';
-
-import Header from './components/Header';
+import Logo from '../public/logo.png'
+import Header from './components/Shared/Header';
 
 import './App.css';
 
@@ -10,11 +10,17 @@ class App extends Component {
         return (
             <div className="container">
                 <Header>
-                    <Link to="/" className="btn btn-default">Home</Link>
-                    <Link to="/projects" className="btn btn-default">Projects</Link>
-                    <Link to="/login" className="btn btn-default">Login</Link>
-                    <Link to="/register" className="btn btn-default">Register</Link>
-                    <Link to="/logout" className="btn btn-default">Logout</Link>
+                
+                <nav className="navbar navbar-inverse ">
+                <a href="#" className="navbar-brand logo-data"> <img alt="Brand" height="80" src={Logo} width="80"/></a>
+                <ul className="nav navbar-nav right-data">
+                   <li> <Link to="/">Home</Link> </li>
+                   <li> <Link to="/projects">Projects</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/logout">Logout</Link></li>
+                    </ul>
+                    </nav>
                 </Header>
                 {this.props.children}
             </div>
--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div className="jumbotron">
-                <h1>Project Manager</h1>
+            <div className="header clearfix">
+                
                 <div>
-                    {this.props.children}
+                   {this.props.children}
                 </div>
             </div>
         );
