6a55c975a7d756afc040c6ee4b0c52c8362b9338
Gornakoff <nikolaygornakov@yahoo.com>
2016-12-06 00:16:16 +0200

    Login/logout behavior

--- src/App.js
+++ src/App.js
@@ -6,25 +6,71 @@ import Header from './components/Shared/Header';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            username: ''
+        };
+        this.onSessionUpdate = this.onSessionUpdate.bind(this);
+    }
+
+    componentDidMount() {
+        this.checkUserCredentials();
+    }
+
+    onSessionUpdate() {
+        this.checkUserCredentials();
+    }
+
+    checkUserCredentials() {
+        let username = sessionStorage.getItem('username');
+        if (!username) {
+            this.setState({
+                loggedIn: false
+            })
+        } else {
+            this.setState({
+                loggedIn: true,
+                username: username
+            })
+        }
+    }
+
     render() {
-        return (
-            <div className="container">
-                <Header>
-                
-                <nav className="navbar navbar-inverse ">
-                <a href="#" className="navbar-brand logo-data"> <img alt="Brand" height="80" src={Logo} width="80"/></a>
-                <ul className="nav navbar-nav right-data">
-                   <li> <Link to="/">Home</Link> </li>
-                   <li> <Link to="/projects">Projects</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/logout">Logout</Link></li>
-                    </ul>
-                    </nav>
-                </Header>
-                {this.props.children}
-            </div>
-        );
+        if (this.state.loggedIn) {
+            return (
+                <div className="container">
+                    <Header>
+                        <nav className="navbar navbar-inverse ">
+                            <a href="#" className="navbar-brand logo-data"> <img alt="Brand" height="80" src={Logo} width="80"/></a>
+                            <ul className="nav navbar-nav right-data">
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/projects">Projects</Link></li>
+                                <li><Link to="/logout">Logout</Link></li>
+                            </ul>
+                        </nav>
+                    </Header>
+                    {this.props.children}
+                </div>
+            );
+        } else {
+            return (
+                <div className="container">
+                    <Header>
+                        <nav className="navbar navbar-inverse ">
+                            <a href="#" className="navbar-brand logo-data"> <img alt="Brand" height="80" src={Logo} width="80"/></a>
+                            <ul className="nav navbar-nav right-data">
+                                <li> <Link to="/">Home</Link> </li>
+                                <li><Link to="/login">Login</Link></li>
+                                <li><Link to="/register">Register</Link></li>
+                            </ul>
+                        </nav>
+                    </Header>
+                    {this.props.children}
+                </div>
+            );
+        }
     }
 }
 
