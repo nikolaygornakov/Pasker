78baa96af994f9ead9a879ccd194dfb2fc89e6e4
Tanyo Georgiev <tanyogeorgiev@yahoo.com>
2016-12-06 22:45:32 +0200

    Project Page Design

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,10 +4,10 @@ export default class Project extends Component {
     render() {
         return (
             <div>
-                <div>Project</div>
-                <div>{this.props.projectname}</div>
-                <div>Description</div>
-                <div>{this.props.description}</div>
+            <div className="panel panel-info col-sm-6 col-md-4">
+                <div className="panel-heading"><h3 className="panel-title">Project: {this.props.projectname}</h3> </div>
+                <div className="panel-body">Description: {this.props.description}</div>
+               </div>
             </div>
         )
     }
